40f5e0ed236863a33bbf9b5daafdc88c87a143d8
anilkumarhv <anilkumarhv04@gmail.com>
2020-02-17 15:23:21 +0530

    initial commit

---
+++ src/com/company/java8/strings/StringCharCount.java
@@ -0,0 +1,22 @@
+package com.company.java8.strings;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+public class StringCharCount {
+    public static void main(String[] args) {
+        String input = "hello how are you";
+
+        Map<Object, Long> frequentChars = Arrays.stream(input.toLowerCase().split("")).filter(s -> !s.equalsIgnoreCase(" ")).collect(
+                Collectors.groupingBy(c -> c, Collectors.counting()));
+        frequentChars.forEach((k, v) -> System.out.println(k + ":" + v));
+
+        String str = "Thequickbrownfoxjumpsoverthelazydog";
+        int size = 3;
+
+        String[] tokens = str.split("(?<=\\G.{" + size + "})");
+        System.out.println(Arrays.toString(tokens));
+    }
+}
